fe47cc1306fe078260a4e9a2f83e8df765fb21dd
Swathi-Shetty-gif <swathishetty9924@gmail.com>
2020-10-14 12:52:43 +0530

    create a regex pattern for pincode

--- pincodeproblem.sh
+++ pincodeproblem.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
 echo "welcome to validation of pincode"
+read -p "enyter your pincode" pincode
+pat='^[0-9]{6}$'
+if [ $pincode =~ $pat ]
+then
+	echo "valid"
+else
+	echo "not valid"
+fi
